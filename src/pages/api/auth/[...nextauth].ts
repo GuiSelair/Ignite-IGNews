b918ac0e0d20b5b7e86fd30e08a12578f0b046da
Guilherme Selair <guilherme.lima1997@hotmail.com>
2022-03-07 20:57:32 -0300

    Feat: Added FaunaDB service

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna"; 
 
 export default NextAuth({
   providers: [
@@ -8,4 +11,41 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     })
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              { data: { 
+                email: user.email,
+                loginCounter: 1
+              }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+         
+        )
+        
+        return true
+      } catch {
+        return false;
+      }
+    }
+  }
 })
