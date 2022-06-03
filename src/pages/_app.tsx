b4d0db7af4b8eaf9b27dbd02db933408e1f2986b
Guilherme Selair <guilherme.lima@getrak.com.br>
2022-06-03 00:31:20 -0300

    Feat: Added prismic config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,31 @@
 import { AppProps } from "next/app";
-import { Header } from "../components/Header";
+import Link from "next/link";
 import { SessionProvider as NextAuthProvider } from "next-auth/react";
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+import { linkResolver, repositoryName } from "../../prismicio";
+
+import { Header } from "../components/Header";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-    <NextAuthProvider session={session} >
-      <Header/>
-        <Component {...pageProps} />
-    </NextAuthProvider>
-  )
+	return (
+		<NextAuthProvider session={session}>
+			<PrismicProvider
+				linkResolver={linkResolver}
+				internalLinkComponent={({ href, children, ...props }): any => {
+					<Link href={href}>
+						<a {...props}>{children}</a>
+					</Link>;
+				}}
+			>
+				<PrismicPreview repositoryName={repositoryName}>
+					<Header />
+					<Component {...pageProps} />
+				</PrismicPreview>
+			</PrismicProvider>
+		</NextAuthProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
